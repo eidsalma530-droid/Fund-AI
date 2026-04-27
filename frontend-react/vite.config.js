dcde9704d8aefc8cdbd96fd13c504a6f76256758
unknown <Ahmed Eid@62o7hBVJWc.62o7hBVJWc>
2026-04-28 01:23:27 +0300

    Final deploy: connected frontend to Hugging Face backend

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fund-AI/', // Add this line here
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
